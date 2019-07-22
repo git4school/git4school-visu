8f555ed64c1bb29ffd46cdc79f9da8e35f61cf16
Mika <mario.32@live.fr>
2019-07-22 23:21:19 +0200

    Refactor: deleted every useless files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
-import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuard } from './services/auth-guard.service';
-import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
-import { UserService } from './services/user.service';
-import { UserListComponent } from './user-list/user-list.component';
-import { NewUserComponent } from './new-user/new-user.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { GraphViewComponent } from './graph-view/graph-view.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { CommitViewComponent } from './commit-view/commit-view.component';
-import { CommitListComponent } from './commit-list/commit-list.component';
 import { CommitsService } from './services/commits.service';
 import * as firebase from 'firebase/app';
 import 'firebase/performance';
@@ -57,22 +46,9 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const appRoutes: Routes = [
-  {
-    path: 'appareils',
-    canActivate: [AuthGuard],
-    component: AppareilViewComponent
-  },
-  {
-    path: 'appareils/:id',
-    canActivate: [AuthGuard],
-    component: SingleAppareilComponent
-  },
-  { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AuthComponent },
   { path: 'not-found', component: FourOhFourComponent },
-  { path: 'users', component: UserListComponent },
-  { path: 'new-user', canActivate: [AuthGuard], component: NewUserComponent },
   {
     path: 'graph',
     canActivate: [AuthGuard],
@@ -94,25 +70,15 @@ const appRoutes: Routes = [
     canActivate: [AuthGuard, DataProvidedGuard],
     component: MetadataViewComponent
   },
-  { path: 'commits', canActivate: [AuthGuard], component: CommitListComponent },
   { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent,
-    AppareilComponent,
     AuthComponent,
-    AppareilViewComponent,
-    SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent,
-    UserListComponent,
-    NewUserComponent,
     GraphViewComponent,
-    CommitViewComponent,
-    CommitListComponent,
     StudentsCommitsViewComponent,
     QuestionsCompletionViewComponent,
     MetadataViewComponent
@@ -142,11 +108,9 @@ const appRoutes: Routes = [
     NgbModule
   ],
   providers: [
-    AppareilService,
     AuthService,
     AuthGuard,
     CommitsService,
-    UserService,
     JsonManagerService,
     DataService
   ],
