cd6b39cdd8295e695eec703146f0a656fd76fade
F0urchette <mario.32@live.fr>
2020-11-09 11:53:50 +0100

    🐛 raise number of repos displayed to 100 (#9)
    Skip review because it's a really simple fix

--- src/app/services/commits.service.ts
+++ src/app/services/commits.service.ts
@@ -483,7 +483,7 @@ export class CommitsService {
   }
 
   getRepositoriesByAuthenticatedUser(): Observable<Repository[]> {
-    let url = 'https://api.github.com/user/repos';
+    let url = 'https://api.github.com/user/repos?per_page=100';
     return this.http.get<any[]>(url, this.httpOptions).pipe(
       map(response => {
         const array = response.map(data => new Repository(data['html_url'], data['name']));
