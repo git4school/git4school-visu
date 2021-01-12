0b90b5069f15b33edfb9db88b9d0ffaf4697fb01
F0urchette <mario.32@live.fr>
2021-01-12 17:38:17 +0100

    🚨 Fix codacy issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })],
+  imports: [
+    RouterModule.forRoot(appRoutes, { relativeLinkResolution: "legacy" }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
