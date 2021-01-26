283b7410e025e0dafaeb49aa13a63ae2ceec10cb
F0urchette <mario.32@live.fr>
2021-01-26 23:02:07 +0100

    ✨ Questions choose improvements Questions chooser can be used in a form Placeholder added Questions chooser can be disabled Message added when there is no questions

--- src/app/components/questions-chooser/questions-chooser.component.ts
+++ src/app/components/questions-chooser/questions-chooser.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, forwardRef, Input, OnInit } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -6,13 +7,21 @@ import { map } from "rxjs/operators";
   selector: "questions-chooser",
   templateUrl: "./questions-chooser.component.html",
   styleUrls: ["./questions-chooser.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => QuestionsChooserComponent),
+      multi: true,
+    },
+  ],
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuestionsChooserComponent implements OnInit {
+export class QuestionsChooserComponent implements OnInit, ControlValueAccessor {
   @Input() questions: string[] = [];
   @Input() questionSuggestions: string[] = [];
   @Input() editable = true;
   @Input() openOnFocus = false;
+  disabled: boolean;
 
   question: string;
 
@@ -24,6 +33,7 @@ export class QuestionsChooserComponent implements OnInit {
         this.questionSuggestions
           .filter(
             (question) =>
+              !this.questions.includes(question) &&
               question.toLowerCase().indexOf(search.toLowerCase()) > -1
           )
           .slice(0, 10)
@@ -31,6 +41,7 @@ export class QuestionsChooserComponent implements OnInit {
     );
 
   ngOnInit(): void {
+    this.disabled = false;
     this.questions = [...this.questions];
     this.questionSuggestions = [...this.questionSuggestions];
   }
@@ -39,14 +50,34 @@ export class QuestionsChooserComponent implements OnInit {
     if (question && !this.questions.includes(question)) {
       this.questions.push(question);
       this.question = null;
+      this.propagateChange(this.questions);
     }
   }
 
   deleteQuestion(index: number) {
     this.questions.splice(index, 1);
+    this.propagateChange(this.questions);
   }
 
   onEnter() {
     this.addQuestion(this.question);
   }
+
+  writeValue(obj: any): void {
+    if (obj) {
+      this.questions = obj;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {}
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  private propagateChange = (_: any) => {};
 }
