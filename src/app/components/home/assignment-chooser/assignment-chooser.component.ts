af771fdf42bbef91074f9306d61d3c17f9e9f8cf
julian.denis <julian.denis-ed@outlook.fr>
2021-05-31 14:42:03 +0200

    ⚗️ groupping rows work in progress

--- src/app/components/home/assignment-chooser/assignment-chooser.component.ts
+++ src/app/components/home/assignment-chooser/assignment-chooser.component.ts
@@ -27,16 +27,6 @@ export class AssignmentChooserComponent implements OnInit {
   assignments: Assignment[];
   modalRef: NgbModalRef;
 
-  funder = [];
-  calculated = [];
-  pending = [];
-  groups = [];
-
-  editing = {};
-  rows = [];
-
-  
-
   constructor(
     private databaseService: DatabaseService,
     private dataService: DataService,
@@ -48,8 +38,7 @@ export class AssignmentChooserComponent implements OnInit {
     public activeModalService: NgbActiveModal,
     private assignmentsService: AssignmentsService,
     private configurationService: ConfigurationService
-  ) {this.assignments = [];
-    this.loadAssignments();}
+  ) {}
 
   ngOnInit(): void {
     this.assignments = [];
@@ -95,6 +84,7 @@ export class AssignmentChooserComponent implements OnInit {
   }
 
   importDB(blob: Blob) {
+    console.log(blob);
     let translations = this.translateService.instant([
       "SUCCESS",
       "ERROR",
@@ -119,6 +109,7 @@ export class AssignmentChooserComponent implements OnInit {
   }
 
   changeListener($event): void {
+    console.log($event);
     let file = $event.target.files[0];
     if (file) {
       this.openImportConfirmation().then(
@@ -133,122 +124,9 @@ export class AssignmentChooserComponent implements OnInit {
     return this.modalRef.result;
   }
 
-  getGroupRowHeight(group, rowHeight) {
-    let style = {};
-
-    style = {
-      height: group.length * 40 + 'px',
-      width: '100%'
-    };
-
-    return style;
-  }
-
-  checkGroup(event, row, rowIndex, group) {
-    let groupStatus = 'Pending';
-    let expectedPaymentDealtWith = true;
-
-    row.exppayyes = 0;
-    row.exppayno = 0;
-    row.exppaypending = 0;
-
-    if (event.target.checked) {
-      if (event.target.value === '0') {
-        // expected payment yes selected
-        row.exppayyes = 1;
-      } else if (event.target.value === '1') {
-        // expected payment yes selected
-        row.exppayno = 1;
-      } else if (event.target.value === '2') {
-        // expected payment yes selected
-        row.exppaypending = 1;
-      }
-    }
-
-    if (group.length === 2) {
-      // There are only 2 lines in a group
-      // tslint:disable-next-line:max-line-length
-      if (
-        ['Calculated', 'Funder'].indexOf(group[0].comment) > -1 &&
-        ['Calculated', 'Funder'].indexOf(group[1].comment) > -1
-      ) {
-        // Sources are funder and calculated
-        // tslint:disable-next-line:max-line-length
-        if (group[0].startdate === group[1].startdate && group[0].enddate === group[1].enddate) {
-          // Start dates and end dates match
-          for (let index = 0; index < group.length; index++) {
-            if (group[index].comment !== row.comment) {
-              if (event.target.value === '0') {
-                // expected payment yes selected
-                group[index].exppayyes = 0;
-                group[index].exppaypending = 0;
-                group[index].exppayno = 1;
-              }
-            }
-
-            if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0) {
-              expectedPaymentDealtWith = false;
-            }
-            console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
-          }
-        }
-      }
-    } else {
-      for (let index = 0; index < group.length; index++) {
-        if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0) {
-          expectedPaymentDealtWith = false;
-        }
-        console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
-      }
-    }
-
-    // check if there is a pending selected payment or a row that does not have any expected payment selected
-    if (
-      group.filter(rowFilter => rowFilter.exppaypending === 1).length === 0 &&
-      group.filter(rowFilter => rowFilter.exppaypending === 0 && rowFilter.exppayyes === 0 && rowFilter.exppayno === 0)
-        .length === 0
-    ) {
-      console.log('expected payment dealt with');
-
-      // check if can set the group status
-      const numberOfExpPayYes = group.filter(rowFilter => rowFilter.exppayyes === 1).length;
-      const numberOfSourceFunder = group.filter(rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Funder')
-        .length;
-      const numberOfSourceCalculated = group.filter(
-        rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Calculated'
-      ).length;
-      const numberOfSourceManual = group.filter(rowFilter => rowFilter.exppayyes === 1 && rowFilter.source === 'Manual')
-        .length;
-
-      console.log('numberOfExpPayYes', numberOfExpPayYes);
-      console.log('numberOfSourceFunder', numberOfSourceFunder);
-      console.log('numberOfSourceCalculated', numberOfSourceCalculated);
-      console.log('numberOfSourceManual', numberOfSourceManual);
-
-      if (numberOfExpPayYes > 0) {
-        if (numberOfExpPayYes === numberOfSourceFunder) {
-          groupStatus = 'Funder Selected';
-        } else if (numberOfExpPayYes === numberOfSourceCalculated) {
-          groupStatus = 'Calculated Selected';
-        } else if (numberOfExpPayYes === numberOfSourceManual) {
-          groupStatus = 'Manual Selected';
-        } else {
-          groupStatus = 'Hybrid Selected';
-        }
-      }
-    }
-
-    group[0].groupstatus = groupStatus;
-  }
-
-  updateValue(event, cell, rowIndex) {
-    this.editing[rowIndex + '-' + cell] = false;
-    this.assignments[rowIndex][cell] = event.target.value;
-    this.assignments = [...this.assignments];
-  }
-
   toggleExpandGroup(group) {
-    console.log('Toggled Expand Group!', group);
+    console.log(this.table, this.table?.groupHeader);
+    console.log('Toggled Expand Group!', group, group.value[0], group.value[1], group.value[0].metadata.startDate);
     this.table.groupHeader.toggleExpandGroup(group);
   }
 
