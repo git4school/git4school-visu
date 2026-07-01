10ae71232a85576cdfea544c70d4745d7177adb0
Mika Pons <mika.pons@irit.fr>
2026-07-01 10:33:27 +0200

    🩹 Fix group text field when editing repo from modal

--- src/app/shared/ui/text-input/text-input.component.ts
+++ src/app/shared/ui/text-input/text-input.component.ts
@@ -36,7 +36,7 @@ export class TextInputComponent implements ControlValueAccessor, OnInit {
   @Input() valid = false;
   @Input() id = "";
 
-  @Input() suggestions: string[] = [];
+  @Input() suggestions?: string[];
 
   @ViewChild("inputElement", { static: false })
   inputElement: ElementRef<HTMLInputElement>;
