15dc9bc78bca376ed0638275816042b8a2b1874a
Mika Pons <mika.pons@irit.fr>
2026-07-07 12:43:43 +0200

    🚧 Trying to reorganize edit milestone modal but not convinced

--- src/app/components/edit-milestone/edit-milestone.component.ts
+++ src/app/components/edit-milestone/edit-milestone.component.ts
@@ -7,12 +7,10 @@ import {
 } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Milestone } from "@models/Milestone.model";
-import { NgbTypeahead } from "@ng-bootstrap/ng-bootstrap";
+import { Observable, Subject, merge } from "rxjs";
 import { CustomModalRef } from "@shared/ui/custom-modal/custom-modal-ref";
 import { Utils } from "@services/utils";
 import * as moment from "moment";
-import { Observable, Subject, merge } from "rxjs";
-import { filter, map } from "rxjs/operators";
 import { TypePickerOption } from "@shared/ui/type-picker/type-picker.component";
 
 @Component({
@@ -29,10 +27,6 @@ export class EditMilestoneComponent implements OnInit {
   @Input() typeaheadSettings;
   @Input() notes: string;
   milestoneForm: FormGroup;
-
-  @ViewChild("instance") instance: NgbTypeahead;
-  focus$ = new Subject<string>();
-  click$ = new Subject<string>();
 
   milestoneTypes: TypePickerOption[] = [
     { value: 'corrections', label: 'CORRECTION', color: 'var(--color-danger)' },
@@ -63,22 +57,6 @@ export class EditMilestoneComponent implements OnInit {
     });
   }
 
-  searchGroup = (text$: Observable<string>) => {
-    const clicksWithClosedPopup$ = this.click$.pipe(
-      filter(() => !this.instance.isPopupOpen())
-    );
-    const inputFocus$ = this.focus$;
-    return merge(text$, clicksWithClosedPopup$, inputFocus$).pipe(
-      map((search) =>
-        (this.tpGroups || [])
-          .filter(
-            (group) =>
-              group.toLowerCase().indexOf((search || "").toLowerCase()) > -1
-          )
-          .slice(0, 10)
-      )
-    );
-  };
 
   deleteMilestone() {
     this.activeModalService.close(null);
